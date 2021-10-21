2f18b85342ccdd264a4048ab63ecb37e3d406a8d
Alberto Araujo <albertoig8@hotmail.com>
2021-10-21 18:54:28 -0300

    Adicionar arquivos de projeto.

---
+++ PokeWebApiApp/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeWebApiApp
+{
+    public class Pokemon
+    {
+        string nome;
+        string tipo;
+        string poderes;
+        double peso;
+        double altura;
+        string elemento;
+        string evolucao;
+        string velocidade;
+    }
+}
