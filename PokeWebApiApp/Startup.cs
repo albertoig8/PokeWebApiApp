27b7575ebe5dba73b01c3e1d4942d822d03570c2
Alberto Araujo <albertoig8@hotmail.com>
2021-10-23 23:21:30 -0300

    Criando metodos para receber os dados

--- PokeWebApiApp/Startup.cs
+++ PokeWebApiApp/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PokeWebApiApp.Models;
 
 namespace PokeWebApiApp
 {
@@ -28,6 +23,7 @@ namespace PokeWebApiApp
         {
 
             services.AddControllers();
+            services.AddDbContext<PokemonContext>(opt => opt.UseInMemoryDatabase("PokermonList"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PokeWebApiApp", Version = "v1" });
